b5754719c22389860ed5961ac6d5f14c3fece7f5
Harinivelu <harinivelu45@gmail.com>
2025-06-13 12:18:44 +0530

    Add files via upload

--- checkbox.spec.js
+++ checkbox.spec.js
@@ -3,10 +3,10 @@ const{test,expect}=require('@playwright/test')
 test('checkbox',async ({page}) =>{
 
     await page.goto('https://testautomationpractice.blogspot.com/')
-    await page.locator('#sunday').check()
+    await page.locator('#tuesday').check()
     await page.waitForTimeout(2000)
-    await expect(page.locator('#sunday').isChecked()).toBeTruthy()
-    await page.locator('#sunday').uncheck()
-    expect (await page.locator('#sunday').isChecked()).toBeFalsy()
+    await expect(page.locator('#tuesday').isChecked()).toBeTruthy()
+    await page.locator('#tuesday').uncheck()
+    expect (await page.locator('#tuesday').isChecked()).toBeFalsy()
 
 })
